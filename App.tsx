57edbcb2fe6c5a2d1af7546f0781b2aed5742a16
Matheus <matheusim27@hotmail.com>
2024-01-07 14:22:11 -0300

    feat: initial project structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from '@components/Loading';
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
+import Home from '@screens/Home';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { StyleSheet, Text } from 'react-native';
+import { THEME } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      {!fontsLoaded ? <Loading /> : <Home />}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
