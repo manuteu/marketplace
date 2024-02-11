72c351367c436634dd08b5d22bc7b02fd95f2974
Matheus <matheusim27@hotmail.com>
2024-02-10 21:42:17 -0300

    fix: product card props

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Box, Button as ButtonNativeBase, HStack, IButtonProps, Image, Text, VStack, View } from 'native-base'
+import { Box, HStack, Image, Pressable, Text, VStack } from 'native-base'
 import Tenis from '@assets/image_tenis.png'
 import User from '@assets/user.png'
+import { InterfacePressableProps } from 'native-base/lib/typescript/components/primitives/Pressable/types'
 
-export type ProductCardProps = {
+export type ProductCardProps = InterfacePressableProps & {
   title: string;
   value: string;
   state: 'new' | 'used';
@@ -11,14 +12,14 @@ export type ProductCardProps = {
   userPhoto: string | undefined;
 }
 
-export default function ProductCard({ title, value, state, status = 'active', userPhoto }: ProductCardProps) {
+export default function ProductCard({ title, value, state, status = 'active', userPhoto, ...rest }: ProductCardProps) {
   return (
-    <Box>
+    <Pressable {...rest}>
       <Image w='auto' h={120} resizeMethod='resize' alt='Product Image' rounded='md' source={Tenis} />
       <Box position='absolute' w='full' p={1} flexDirection='row' justifyContent='space-between' >
         <Image borderWidth={1} borderColor='gray.100' alt='Product Image' rounded='full' source={User} />
-        <Box py={1} px={3} bg={state === 'new' ? 'blue.500' : 'gray.600'} rounded='full'>
-          <Text fontFamily='bold' fontSize='xs' color='gray.100' right={0} textTransform='uppercase'>{state === 'new' ? 'NOVO' : 'USADO'}</Text>
+        <Box px={2} h={17} alignItems='center' flexDirection='row' bg={state === 'new' ? 'blue.500' : 'gray.600'} rounded='full'>
+          <Text fontFamily='bold' fontSize='2xs' color='gray.100' right={0} textTransform='uppercase'>{state === 'new' ? 'NOVO' : 'USADO'}</Text>
         </Box>
       </Box>
       {status === 'disabled' && (
@@ -34,6 +35,6 @@ export default function ProductCard({ title, value, state, status = 'active', us
           <Text fontFamily='bold' fontSize='md' color={status === 'active' ? 'gray.700' : 'gray.400'}>{value}</Text>
         </HStack>
       </VStack>
-    </Box>
+    </Pressable>
   )
 }
