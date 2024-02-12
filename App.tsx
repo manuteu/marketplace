72915f2f6110360e6057078373ae70b64e12918d
Matheus <matheusim27@hotmail.com>
2024-02-12 16:56:40 -0300

    fix: app statusbar color

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
 import Loading from '@components/Loading';
 import { useFonts, Karla_300Light , Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
-import Home from '@screens/Home';
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import { StyleSheet, Text } from 'react-native';
 import { THEME } from './src/theme';
-import SignIn from '@screens/SignIn';
-import SignUp from '@screens/SignUp';
 import { Routes } from '@routes/intex';
 
 export default function App() {
@@ -14,7 +10,7 @@ export default function App() {
     <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle='dark-content'
-        backgroundColor="transparent"
+        backgroundColor="#EDECEE"
         translucent
       />
       {!fontsLoaded ? <Loading /> : <Routes />}
