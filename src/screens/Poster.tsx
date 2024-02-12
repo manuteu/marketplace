3345bf46b8aa638830066bfb7a56f26102513e84
Matheus <matheusim27@hotmail.com>
2024-02-12 16:57:06 -0300

    feat: carousel

--- src/screens/Poster.tsx
+++ src/screens/Poster.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView, } from 'react-native-safe-area-context'
 import { Box, HStack, Image, Pressable, ScrollView, Text } from 'native-base'
 import { Feather } from '@expo/vector-icons'
@@ -8,21 +8,58 @@ import Bike from '@assets/bike.png'
 import ProductContent from '@components/ProductContent'
 import Button from '@components/Button'
 import WppSvg from '@assets/wpp_icon.svg'
+import Carousel from 'react-native-reanimated-carousel'
+import { Dimensions } from 'react-native'
 
 export default function Poster() {
   const { navigate } = useNavigation<StackNavigatorRoutesProps>()
+  const width = Dimensions.get('window').width;
+  const [carouselData, setcarouselData] = useState([
+    { img: Bike, id: 0, active: true },
+    { img: Bike, id: 1, active: false },
+    { img: Bike, id: 2, active: false },
+  ])
+
+  const handleSwipeImage = (index: number) => {
+    const newCarouselData = carouselData.map((item) => {
+      if (item.id === index) {
+        item.active = true
+      } else {
+        item.active = false;
+      }
+      return item;
+    })
+    setcarouselData(newCarouselData)
+  }
 
   return (
-    <SafeAreaView style={{ paddingTop: 24 }}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-
+    <SafeAreaView style={{ paddingTop: 24, flex: 1, backgroundColor: '#EDECEE' }}>
+      <ScrollView showsVerticalScrollIndicator={false} >
         <HStack px={6} h={14} alignItems='center' justifyContent='space-between'>
           <Pressable onPress={() => navigate('stackHome')}>
             <Feather name="arrow-left" size={24} color="#1A181B" />
           </Pressable>
         </HStack>
 
-        <Image w='full' alt='bike_image' source={Bike} />
+        <Box width={width} height={width / 3 * 2}>
+          <Carousel
+            loop
+            width={width}
+            height={width / 3 * 2}
+            data={carouselData}
+            scrollAnimationDuration={1000}
+            onSnapToItem={(index) => handleSwipeImage(index)}
+            renderItem={({ index }) => (
+              <Image key={index} w='full' alt='bike_image' source={Bike} />
+            )}
+          />
+          <HStack px={1} space={2} w='full' position='absolute' bottom={1}>
+            {carouselData.map((item) => (
+              <Box flex={1} height={1} rounded='full' bg='gray.100' opacity={item.active ? 80 : 50} />
+            ))}
+          </HStack>
+        </Box>
+
         <ProductContent />
       </ScrollView>
       <Box position='absolute' w='full' bg='gray.100' bottom={0} h={90} justifyContent='center' pb={2}>
