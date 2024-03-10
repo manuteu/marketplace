61840c16a16322d2ecef9562bbd68b4360d537b4
Matheus <matheusim27@hotmail.com>
2024-03-10 11:17:26 -0300

    fix: SSR log

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import Loading from '@components/Loading';
 import { useFonts, Karla_300Light , Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/theme';
-import { Routes } from '@routes/intex';
+import { Routes } from '@routes/index';
+import { LogBox } from "react-native"
+LogBox.ignoreLogs([ 'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app.', ])
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_300Light, Karla_400Regular, Karla_700Bold });
