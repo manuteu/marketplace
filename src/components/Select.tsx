28cae54f8e2115eca03a2ffd541451372f53d262
Matheus <matheusim27@hotmail.com>
2024-04-10 12:24:02 -0300

    feat: filter and skeleton my producs

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import { Select as NativeSelect } from 'native-base'
 import { Feather } from '@expo/vector-icons';
 
-export default function Select() {
-  const [service, setService] = useState("");
+type Props = {
+  filter: string;
+  setFilter: (value: string) => void;
+}
+
+export default function Select({ filter, setFilter }: Props) {
   return (
     <NativeSelect
-      selectedValue={service}
+      selectedValue={filter}
       defaultValue='all'
       minWidth="120"
       accessibilityLabel="Todos"
@@ -14,13 +18,13 @@ export default function Select() {
       _selectedItem={{
         bg: "gray.200",
       }}
-      onValueChange={itemValue => setService(itemValue)}
-      dropdownIcon={<Feather name="chevron-down" size={22} color="#5F5B62" style={{marginRight: 8}} />}
+      onValueChange={itemValue => setFilter(itemValue)}
+      dropdownIcon={<Feather name="chevron-down" size={22} color="#5F5B62" style={{ marginRight: 8 }} />}
       fontSize='sm'
       borderRadius={6}
       px={2}
       py={1}
-      >
+    >
       <NativeSelect.Item label="Todos" value="all" />
       <NativeSelect.Item label="Novo" value="new" />
       <NativeSelect.Item label="Usado" value="used" />
--- src/screens/MyPosters.tsx
+++ src/screens/MyPosters.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Box, FlatList, HStack, ScrollView, Text, VStack, useToast } from 'native-base'
+import { Box, FlatList, HStack, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import Select from '@components/Select'
@@ -14,16 +14,18 @@ import { ProductDTO } from '@dtos/ProductDTO'
 
 export default function MyPosters() {
   const { navigate } = useNavigation<StackNavigatorRoutesProps>()
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState<ProductDTO[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<ProductDTO[]>([])
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [filter, setFilter] = useState('')
   const toast = useToast()
 
   const handleGetPosters = async () => {
     setLoading(true)
     try {
       const { data } = await api.get(`/users/products`)
-
+      setFilteredProducts(data)
       setProducts(data)
     } catch (error) {
       const isAppError = error instanceof AppError
@@ -62,6 +64,16 @@ export default function MyPosters() {
     handleGetPosters()
   }, []))
 
+  useEffect(() => {
+    const filtered = products.filter(product =>
+      filter === 'new' ?
+        product.is_new === true
+        : filter === 'used' ?
+          product.is_new === false : product)
+
+    setFilteredProducts(filtered);
+  }, [filter])
+
   return (
     <SafeAreaView style={{ paddingTop: 24 }}>
       <VStack px={6}>
@@ -75,26 +87,44 @@ export default function MyPosters() {
 
         <HStack mt={4} mb={2} alignItems='center' justifyContent='space-between'>
           <Text fontFamily='regular' fontSize='sm' color='gray.600'>{products.length} {products.length === 1 ? 'anúncio' : 'anúncios'}</Text>
-          <Select />
+          <Select filter={filter} setFilter={setFilter} />
         </HStack>
-        <FlatList
-          pt={4}
-          data={products}
-          renderItem={({ item }) => <ProductCard onPress={() => navigate('poster', { data: { ...item }, type: 'buyPoster' })} {...item} />}
-          keyExtractor={({ id }) => id}
-          numColumns={2}
-          contentContainerStyle={{
-            paddingBottom: 180,
-            gap: 24,
-          }}
-          columnWrapperStyle={{
-            gap: 20,
-          }}
-          showsVerticalScrollIndicator={false}
-          ListEmptyComponent={() => <Text></Text>}
-          onRefresh={handleRefresh}
-          refreshing={isRefreshing}
-        />
+        {loading ? (
+          <VStack pt={4} w='100%' flexDirection='row' space={2} flexWrap='wrap'>
+            {[1, 2, 3, 4].map((skeleton, index) => {
+              const itemStyle = index % 2 === 0 ? { paddingRight: 10 } : { paddingLeft: 10 };
+              return (
+                <VStack key={index} w='1/2' style={itemStyle} space={2} mb={5}>
+                  <Skeleton h="120" rounded='md' startColor='gray.200' endColor='gray.300' />
+                  <Box position='absolute' top={2} right={index % 2 === 0 ? 4 : 2} >
+                    <Skeleton h='17' w={10} rounded='full' startColor='gray.200' endColor='gray.200' />
+                  </Box>
+                  <Skeleton h='3' rounded='md' startColor='gray.200' endColor='gray.300' />
+                  <Skeleton h='3' w='1/2' rounded='md' startColor='gray.200' endColor='gray.300' />
+                </VStack>
+              )
+            })}
+          </VStack>
+        ) : (
+          <FlatList
+            pt={4}
+            data={filteredProducts}
+            renderItem={({ item }) => <ProductCard onPress={() => navigate('poster', { data: { ...item }, type: 'buyPoster' })} {...item} />}
+            keyExtractor={({ id }) => id}
+            numColumns={2}
+            contentContainerStyle={{
+              paddingBottom: 180,
+              gap: 24,
+            }}
+            columnWrapperStyle={{
+              gap: 20,
+            }}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={() => <Text>Nenhum produto anunciado...</Text>}
+            onRefresh={handleRefresh}
+            refreshing={isRefreshing}
+          />
+        )}
       </VStack>
     </SafeAreaView>
   )
